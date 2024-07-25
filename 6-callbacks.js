fa8b610bcd734c6dc9e1cc4169f634f4887ca502
Théfa <140509878+Thefa07@users.noreply.github.com>
2024-07-25 11:24:38 -0300

    Add files via upload

---
+++ 6-callbacks.js
@@ -0,0 +1,12 @@
+const nomes = ["Evaldo", "Mari", "Camis"];
+nomes.forEach(function(nome){
+    console.log(nome)
+})
+
+nomes.forEach(function(){})
+nomes.forEach(() =>{})
+
+function imprimeNome(nome) {
+    console.log(nome);
+}
+nomes.forEach(imprimeNome)
