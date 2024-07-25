b7cbae5035489124ce791173e26edfd7a03a96a6
Théfa <140509878+Thefa07@users.noreply.github.com>
2024-07-25 11:52:14 -0300

    Add files via upload

---
+++ 18.js
@@ -0,0 +1,7 @@
+const nome = "Théfinha";
+let noneMaiusculas = "";
+
+for (let i= 0; i < nome.length; i++){
+    nomeMaiusculas = nomeMaiusculas + nome [i].toUpperCase()
+}
+console.log(nomeMaiusculas)
---
+++ 19.js
@@ -0,0 +1,6 @@
+const arrayNums = [1, 2, 3, 4]
+function multiplicaPorDez(num){
+    return num * 10
+}
+const arraySomada = arrayNum.map(multiplicaPorDez)
+console.log(arraySomada)
